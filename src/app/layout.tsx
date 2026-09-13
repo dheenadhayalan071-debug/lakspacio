0c306742020604e8d4b8db77ce85710d4cfd5b1c
dheenadhayalan071-debug <dheenadhayalan071@gmail.com>
2026-09-13 19:16:21 +0530

    Fix import path for Navigation component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import "./globals.css";
-import Navigation from "@/components/Navigation";
+import Navigation from "../components/Navigation";
 
 export const metadata = {
   title: "LAKSPACIO | Intelligent Access to Sports Space",
